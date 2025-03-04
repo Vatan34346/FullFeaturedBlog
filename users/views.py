ef3092858c3fc79e5f76f1888cf59eb5f0294cfa
Vatan34346 <paatagvichia@gmail.com>
2025-03-04 17:25:49 +0400

    added pagination Crud for posts, edited some profile links

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
@@ -21,5 +21,22 @@ def register(req):
 
 @login_required
 def profile(req):
-    return render(req, 'users/profile.html')
+    if req.method == 'POST':
+        u_form = UserUpdateForm(req.POST, instance=req.user)
+        p_form = ProfileUpdateForm(req.POST, req.FILES, instance=req.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(req, f"Your account has been updated!")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=req.user)
+        p_form = ProfileUpdateForm(instance=req.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form':p_form
+    }
+    return render(req, 'users/profile.html', context)
 
